e82f09145bbcc6f3f278f89f7a09461be68a477f
opmiss <opmiss@gmail.com>
2014-03-13 22:57:18 -0400

    commit

---
+++ script/link.js
@@ -0,0 +1,6 @@
+document.write(" <li><a href=\" http\:\/\/www.gvu.gatech.edu/people/official/jarek/magic \"> Magic Lab </a></li> ");
+document.write(" <li><a href=\" http\:\/\/www.ic.gatech.edu \"> Gatech Interactive Computing </a></li> ");
+document.write(" <li><a href=\" http\:\/\/www.waston.ibm.com \"> IBM TJ Waston Research </a></li> ");
+document.write(" <li><a href=\" http\:\/\/vis.lbl.gov \"> LBNL Visualization </a></li> ");
+document.write(" <li><a href=\" http\:\/\/www.huamin.org \"> HKUST Visualization </a></li> ");
+
