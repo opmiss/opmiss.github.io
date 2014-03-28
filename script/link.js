35204987fe9554052836f41a38db144ab20f04b5
opmiss <opmiss@gmail.com>
2014-03-28 13:26:29 -0400

    3-28-2014

--- script/link.js
+++ script/link.js
@@ -1,6 +1,9 @@
+
+
+document.write(" <li><a href=\" http\:\/\/www.processing.org \"> Processing </a></li> ");
+
+document.write(" <li><a href=\" http\:\/\/www.povray.org \"> POV-Ray </a></li> ");
+
 document.write(" <li><a href=\" http\:\/\/www.gvu.gatech.edu/people/official/jarek/magic \"> Magic Lab </a></li> ");
 document.write(" <li><a href=\" http\:\/\/www.ic.gatech.edu \"> Gatech Interactive Computing </a></li> ");
-document.write(" <li><a href=\" http\:\/\/www.waston.ibm.com \"> IBM TJ Waston Research </a></li> ");
-document.write(" <li><a href=\" http\:\/\/vis.lbl.gov \"> LBNL Visualization </a></li> ");
-document.write(" <li><a href=\" http\:\/\/www.huamin.org \"> HKUST Visualization </a></li> ");
 
