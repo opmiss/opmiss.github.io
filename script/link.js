f14639a696ecd371ac9a750dd92e0d00b202a9c9
opmiss <opmiss@gmail.com>
2014-03-28 20:09:36 -0400

    03-28-2014

--- script/link.js
+++ script/link.js
@@ -2,7 +2,7 @@
 
 document.write(" <li><a href=\" http\:\/\/www.processing.org \"> Processing </a></li> ");
 
-document.write(" <li><a href=\" http\:\/\/www.povray.org \"> POV-Ray </a></li> ");
+document.write(" <li><a href=\" http\:\/\/get.webgl.org \"> WebGL </a></li> ");
 
 document.write(" <li><a href=\" http\:\/\/www.gvu.gatech.edu/people/official/jarek/magic \"> Magic Lab </a></li> ");
 document.write(" <li><a href=\" http\:\/\/www.ic.gatech.edu \"> Gatech Interactive Computing </a></li> ");
