3b825322e0f88a195feec52df9de3b5ce1822f9e
opmiss <opmiss@gmail.com>
2014-11-14 21:09:21 -0800

    add dissertation page

---
+++ script/navi.js
@@ -0,0 +1,5 @@
+document.write("<div class=\"navi\"><a href=\"index.html\">Home</a></div>");
+document.write("<div class=\"navi\"><a href=\"Research.html\">Research</a></div>");
+document.write("<div class=\"navi\"><a href=\"Publication.html\">Publication</a></div>");
+document.write("<div class=\"navi\"><a href=\"Dissertation.html\">Dissertation</a></div>");
+
