2884966ddb8355b4a0c8e31bb275312734a3541d
Wei Zhuo <opmiss@gmail.com>
2015-05-12 08:55:31 +0800

    Update header.js

--- script/header.js
+++ script/header.js
@@ -1,3 +1,3 @@
 document.write("<h2>Wei (Tina) Zhuo</h2> <br> ");
 document.write("<p>School of Interactive Computing <br> Georgia Institute of Technology <br> Atlanta, GA 30332<br> ")
-document.write("tina.zhuo@('gatech.' + 'edu')</p>");
+document.write("opmiss@('gmail.' + 'com')</p>");
