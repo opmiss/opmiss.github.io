90986e379f1ace5a0e2add6d67db97a648fb5936
opmiss <opmiss@gmail.com>
2015-09-25 10:31:46 -0700

    add selection

--- demo/VisEditor/model.js
+++ demo/VisEditor/model.js
@@ -1,14 +1,43 @@
 var Rect = function(obj){
   this.show = function(ctx){
+    if (obj.color) {
+      ctx.fillStyle=obj.color;
+    }
     ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
+    if (obj.pick||obj.drag){
+      if (isIn()){
+        ctx.strokeRect(obj.x, obj.y, obj.w, obj.h);
+        if (isDrag&&obj.drag){
+          obj.x = mx-obj.w/2; obj.y = my-obj.h/2;
+        }
+      }
+    }
+  }
+  this.isIn(){
+    return (mx>obj.x && mx<obj.x+obj.w && my>obj.y && my<obj.y+obj.h)
   }
 }
 
 var Circle = function(obj){
   this.show = function(ctx){
+    if (obj.color){
+      ctx.strokeStyle=obj.color;
+    }
     ctx.beginPath();
     ctx.arc(obj.x, obj.y, obj.r, 0, 2*Math.PI);
-    ctx.stroke(); 
+    ctx.stroke();
+    if (obj.pick||obj.drag){
+      if (isIn()){
+        ctx.fill();
+        if (isDrag&&obj.drag){
+          obj.x = mx-obj.w/2; obj.y = my-obj.h/2;
+        }
+      }
+    }
+  }
+  this.isIn(){
+    var dx = mx-obj.x, dy = my-obj.y;
+    return (dx*dx+dy*dy<obj.r*obj.r);
   }
 }
 var MMap=new Object();
